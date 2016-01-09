5cfea80fb2d6807d9e31a76de3cdcaf4c2a20749
Tamesh Sivaguru <tam.s89@gmail.com>
2016-01-09 15:33:47 +0000

    CV Update.

--- CV/CV/wwwroot/_references.js
+++ CV/CV/wwwroot/_references.js
@@ -5,3 +5,5 @@
 /// <reference path="js/debouncer.js" />
 /// <reference path="js/enterprise.js" />
 /// <reference path="js/site.min.js" />
+/// <reference path="lib/bootstrap/dist/js/bootstrap.js" />
+/// <reference path="lib/jquery/dist/jquery.js" />
