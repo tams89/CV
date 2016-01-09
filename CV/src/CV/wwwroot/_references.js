4262d4ed6fe685087da36c6666bc4b0ebf9fa844
Tamesh Sivaguru <tam.s89@gmail.com>
2016-01-09 22:34:45 +0000

    files added.

--- CV/src/CV/wwwroot/_references.js
+++ CV/src/CV/wwwroot/_references.js
@@ -4,5 +4,6 @@
 /// <reference path="js/color-switch.js" />
 /// <reference path="js/debouncer.js" />
 /// <reference path="js/enterprise.js" />
+/// <reference path="js/site.min.js" />
 /// <reference path="lib/bootstrap/dist/js/bootstrap.js" />
 /// <reference path="lib/jquery/dist/jquery.js" />
